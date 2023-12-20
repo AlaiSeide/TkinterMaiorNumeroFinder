5d3e98931ea366e01a3ef35ede0a3db00cdb166d
Alai Seide <154024184+AlaiSeide@users.noreply.github.com>
2023-12-20 21:50:32 +0100

    deploy inicial

---
+++ python-numero-maior-tkinter.py
@@ -0,0 +1,44 @@
+import tkinter as tk
+
+def encontrar_maior():
+    num1 = int(entry1.get())
+    num2 = int(entry2.get())
+    num3 = int(entry3.get())
+
+    maior = num1
+
+    if num2 > maior:
+        maior = num2
+
+    if num3 > maior:
+        maior = num3
+
+    resultado.config(text=f"O maior número é: {maior}")
+
+janela = tk.Tk()
+
+label1 = tk.Label(janela, text="Digite o primeiro número:")
+label1.pack()
+
+entry1 = tk.Entry(janela)
+entry1.pack()
+
+label2 = tk.Label(janela, text="Digite o segundo número:")
+label2.pack()
+
+entry2 = tk.Entry(janela)
+entry2.pack()
+
+label3 = tk.Label(janela, text="Digite o terceiro número:")
+label3.pack()
+
+entry3 = tk.Entry(janela)
+entry3.pack()
+
+botao = tk.Button(janela, text="Encontrar Maior", command=encontrar_maior)
+botao.pack()
+
+resultado = tk.Label(janela, text=" ")
+resultado.pack()
+
+janela.mainloop()
